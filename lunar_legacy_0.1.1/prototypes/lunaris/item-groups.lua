480f1659907a1a7a1722ff7e77e89c16b6ed95b0
DoneTax <alphatryhardx@gmail.com>
2025-04-16 21:29:38 +0200

    Delete lunar_legacy_0.1.1 directory

--- lunar_legacy_0.1.1/prototypes/lunaris/item-groups.lua
+++
@@ -1,8 +0,0 @@
-data:extend({
-  {
-  type = "item-subgroup",
-  name = "lunaris-tiles",
-  group = "tiles",
-  order = "b"
-  },
-})
--- lunar_legacy_0.1.1/prototypes/lunaris/tile/tile-pollution-values.lua
+++
@@ -1,9 +0,0 @@
-return
-{
-  out_of_map = { pollution = 0.0001   },
-  water =      { pollution = 0.000025 },
-  grass =      { pollution = 0.000018 },
-  dirt =       { pollution = 0.000018 },
-  sand =       { pollution = 0.000015 },
-  red_desert = { pollution = 0.000015 }
-}
